edd8306a115ade30912791818708c53b85c771ed
alisalali <alis.alali@hotmail.com>
2024-10-30 11:22:40 +0300

    05-setting-timers-managing-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenges from "./components/TimerChallenges.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenges title={"Easy"} targetTime={1} />
+        <TimerChallenges title={"Not easy"} targetTime={5} />
+        <TimerChallenges title={"Getting Though"} targetTime={15} />
+        <TimerChallenges title={"Pros only"} targetTime={15} />
+      </div>
     </>
   );
 }
