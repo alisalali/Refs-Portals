75ee44c08da7198f2100d99c624ebc6a65bba4d1
alisalali <alis.alali@hotmail.com>
2024-10-31 11:29:51 +0300

    fix ref dialog

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,12 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { result, targetTime },
   ref
 ) {
   const dialog = useRef();
+
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -13,8 +15,8 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
-  return (
-    <dialog ref={ref} className="result-modal">
+  return createPortal(
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
@@ -25,7 +27,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog">
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
